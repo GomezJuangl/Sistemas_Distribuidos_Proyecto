0b755ad2a233f9184ac6a34264d349b7293f2524
Xamuel Perez <xamuel.perez@gmail.com>
2026-04-02 16:06:56 -0500

    Add Broker publication socket

--- PC1/Broker.py
+++ PC1/Broker.py
@@ -3,14 +3,19 @@ import zmq
 class Broker:
     def __init__(self):
         self.context = zmq.Context()
-        self.socket = self.context.socket(zmq.SUB)
-        self.socket.bind("tcp://*:5555")
-        self.socket.setsockopt_string(zmq.SUBSCRIBE, "")
+
+        self.sub_socket = self.context.socket(zmq.SUB)
+        self.sub_socket.bind("tcp://*:5555")
+        self.sub_socket.setsockopt_string(zmq.SUBSCRIBE, "")
+
+        self.pub_socket = self.context.socket(zmq.PUB)
+        self.pub_socket.bind("tcp://*:5556")
 
     def run(self):
         while True:
-            mensaje = self.socket.recv_string()
-            print("Recibido:", mensaje)
+            mensaje = self.sub_socket.recv_string()
+            self.pub_socket.send_string(mensaje)
+            print("Recibido y Enviado:", mensaje)
 
 if __name__ == "__main__":
     broker = Broker()
